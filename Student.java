768c80ffe403dead1e1093d1e52b3b65d58a511f
Nyima <240119nyima@cosmoscollege.edu.np>
2026-07-06 18:38:31 +0545

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Student {
+	    private String name;
+	    private String email;
+
+	    public void setName(String n) {
+	        name = n;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setEmail(String e) {
+	        email = e;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+	}
+
+
