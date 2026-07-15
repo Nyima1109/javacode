f4e6e2cd889e39e15e129278d105834230d244af
Nyima <240119nyima@cosmoscollege.edu.np>
2026-07-15 15:43:56 +0545

    Add files via upload

---
+++ SqlDemo.java
@@ -0,0 +1,10 @@
+package lab7;
+import java.Sql.*;
+public class SqlDemo {
+	public static void main(String args[])throws Exception
+	{
+		class forName("Org.postgresql.Ddriver");
+		Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydb";"user","password");
+		String sql
+	}
+}
